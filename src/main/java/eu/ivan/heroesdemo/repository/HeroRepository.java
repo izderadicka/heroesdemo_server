13d374d31606190c6455f690e2abf0b2c3321f3c
Ivan <ivan.zderadicka@gmail.com>
2021-08-07 15:38:28 +0200

    comment

--- src/main/java/eu/ivan/heroesdemo/repository/HeroRepository.java
+++ src/main/java/eu/ivan/heroesdemo/repository/HeroRepository.java
@@ -15,6 +15,7 @@ public interface HeroRepository extends JpaRepository<Hero, Long> {
 	@Query("from Hero h where lower(h.name) like concat('%', lower(?1), '%') order by h.name")
 	public List<Hero> findByName(String token);
 	
+	//TODO: This is very inefficient way how to get random row, also PG dependent
 	@Query(value = "select * from heroes order by random() limit 4", nativeQuery = true)
 	public List<Hero> findRandom();
 	
