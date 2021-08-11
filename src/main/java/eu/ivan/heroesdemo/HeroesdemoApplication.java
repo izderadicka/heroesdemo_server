1862d7ddee8a55f9ece61554e38b3109763f3b36
Ivan <ivan.zderadicka@gmail.com>
2021-08-11 11:46:06 +0200

    add support for embeded angular application

--- src/main/java/eu/ivan/heroesdemo/HeroesdemoApplication.java
+++ src/main/java/eu/ivan/heroesdemo/HeroesdemoApplication.java
@@ -1,10 +1,16 @@
 package eu.ivan.heroesdemo;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 
 @Configuration
@@ -22,6 +28,22 @@ public class HeroesdemoApplication implements WebMvcConfigurer{
         .allowedOriginPatterns("http://localhost:*");
         
         
+    }
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+ 
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
     }
 
 }
